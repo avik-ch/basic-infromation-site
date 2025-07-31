58443ed5fd083a3b50bc175b6068aa13e91746bf
avik-ch <181900631+avik-ch@users.noreply.github.com>
2025-07-31 10:45:39 -0500

    Add comments for clarity

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 // listen to port
 app.listen(8080);
 
+// routing
 app.get("/", (req, res) => {
   res.sendFile("./views/index.html", { root: __dirname });
 });
@@ -18,6 +19,7 @@ app.get("/contact-me", (req, res) => {
   res.sendFile("./views/contact-me.html", { root: __dirname });
 });
 
+// 404, i.e. it runs when all other pages were not found
 app.use((req, res) => {
   res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
